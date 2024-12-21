11b687eaded677e6c03f4816a346d4c24d1fa586
Revanth Sai Chaparala <98806472+revanthsaich@users.noreply.github.com>
2024-12-21 17:49:06 +0530

    New

--- backend/settings.py
+++ backend/settings.py
@@ -124,11 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    BASE_DIR / "frontend/build/static",  # Adjust to match the build location
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Directory where collectstatic will copy files
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
