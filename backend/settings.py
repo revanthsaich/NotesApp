ac38ec4a355f4d851fdf3fd65621e7221d6fe2cb
Revanth Sai Chaparala <98806472+revanthsaich@users.noreply.github.com>
2024-12-21 18:31:56 +0530

    sadasda

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"  # Directory where collectstatic will copy files
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
